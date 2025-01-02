b9602b97073f0fd401fd555037e12a1ba0df032c
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-01-02 20:27:55 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/HeaderSection/Header";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Feed from "./components/FeedSection/Feed";
+import SearchVideoResult from "./components/SearchSection/SearchVideoResult";
+import VideoDetails from "./components/VideoSection/VideoDetails";
+import { AppContext } from "./useContextHook/useContextApi";
+import { ThemeProvider } from "./useContextHook/useTheme";
 
-import Header from "./components/Header";
-import Feed from "./components/Feed";
-import SearchResult from "./components/SearchResult";
-import VideoDetails from "./components/VideoDetails";
-import { AppContext } from "./context/contextApi";
-
-const App = () => {
-    return (
-        <AppContext>
-            <BrowserRouter>
-                <div className="flex flex-col h-full">
-                    <Header />
-                    <Routes>
-                        <Route path="/" exact element={<Feed />} />
-                        <Route
-                            path="/searchResult/:searchQuery"
-                            element={<SearchResult />}
-                        />
-                        <Route path="/video/:id" element={<VideoDetails />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </AppContext>
-    );
-};
+function App() {
+  return (
+    <AppContext>
+      <ThemeProvider>
+    <BrowserRouter>
+    <div className="flex flex-col w-full">
+      <Header/>
+       <Routes>
+        <Route path="/" element={<Feed/>}/>
+        <Route path="/search/:searchQuery" element={<SearchVideoResult/>}/>
+        <Route path="/video/:categoryId/:videoId" element={<VideoDetails/>}/>
+       </Routes>
+    </div>
+    </BrowserRouter>
+    </ThemeProvider>
+    </AppContext>
+  );
+}
 
 export default App;
